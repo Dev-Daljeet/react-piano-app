cfb45ed1be85f618da242fade41d4dbab0410d5e
Dev-Daljeet <sin10510@sheridancollege.ca>
2021-03-14 10:35:42 -0400

    Restructured the project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,12 @@
 import React from "react";
 import "../css/App.css";
 
-class Header extends React.Component
-{
-    constructor(props)
-    {
-        super(props);
-    }
-
-    render()
-    {
-        return (
-            <header className="header-style">
-            <h1>{this.props.title}</h1>
-            </header>
-        );
-    }
+function Header(props) {
+    return (
+    <header className="header-style">
+    <h1>{props.title}</h1>
+    </header>
+    );
 }
 
 export default Header;
